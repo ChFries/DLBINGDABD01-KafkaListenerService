a1f07bef5c67a1e8b1d412a990a5231cfd704a08
chris <ch.fries@gmx.net>
2024-12-22 12:08:20 +0100

    feat(00): Initial commit KafkaListenerService

---
+++ src/main/java/chfr/iubh/dlbingdabd01kafkalistenerservice/Dlbingdabd01KafkaListenerServiceApplication.java
@@ -0,0 +1,13 @@
+package chfr.iubh.dlbingdabd01kafkalistenerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dlbingdabd01KafkaListenerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dlbingdabd01KafkaListenerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/chfr/iubh/dlbingdabd01kafkalistenerservice/Dlbingdabd01KafkaListenerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package chfr.iubh.dlbingdabd01kafkalistenerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dlbingdabd01KafkaListenerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
